054da3c02e30ddefc3c1988b1d20a99a55086199
soonjongHeo <soon9086@gmail.com>
2023-03-20 17:01:50 +0900

    Update settings.py

--- Jinstagram/settings.py
+++ Jinstagram/settings.py
@@ -78,13 +78,23 @@ WSGI_APPLICATION = 'Jinstagram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '3.39.239.80',
+        'NAME': 'djangoinsta',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
